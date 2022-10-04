d922299d970add70cafa5f2d4279edfa66eef99f
Lynette Lopez <lynettelopez12@gmail.com>
2022-10-04 00:03:24 -0500

    Add a light theme

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,11 +1,20 @@
-import { NextUIProvider } from '@nextui-org/react';
+import { NextUIProvider, createTheme } from '@nextui-org/react';
 import type { AppProps } from 'next/app';
 
 import '../styles/globals.scss';
 
+const lightTheme = createTheme({
+  type: 'light',
+  theme: {
+    colors: {
+      background: '#F8F9FA',
+    },
+  },
+});
+
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider>
+    <NextUIProvider theme={lightTheme}>
       <Component {...pageProps} />
     </NextUIProvider>
   );
