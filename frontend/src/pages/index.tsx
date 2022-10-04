d960b5526a406e19fbcdbc10825af95ad5df1bb0
Lynette Lopez <lynettelopez12@gmail.com>
2022-10-04 00:02:12 -0500

    Remove redundant favicon link

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -8,7 +8,6 @@ export default function Index() {
       <Head>
         <title>Prologue - Writing modernized</title>
         <meta name="description" content="Minimalist writing platform." />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
