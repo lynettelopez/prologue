dd202d8bf4f5a5ab1005011c332475d9ee1e3685
Lynette Lopez <lynettelopez12@gmail.com>
2022-10-03 22:28:00 -0500

    Add `Document` with Google Fonts

---
+++ frontend/src/pages/_document.tsx
@@ -0,0 +1,34 @@
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from "next/document";
+
+export default class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return initialProps;
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;900&family=Lora:ital,wght@0,400;0,600;1,400;1,600&display=optional"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
